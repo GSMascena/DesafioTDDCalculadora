7878c53e6991afb5467889786768bb77353aa62e
gsmascena <gabriel.s.mascena@gmail.com>
2023-11-12 01:16:25 -0300

    first commit

---
+++ TDDCalculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
